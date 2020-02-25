e8ec0c642a8cf5bdb7404673551b14f5704b524c
Kartik Mahajan <kartikmahajan@Kartiks-MacBook-Pro.local>
2020-02-25 00:06:40 -0800

    Data manager minor updates

--- BKTSRC/BKTSRC/IDataManager.cs
+++ BKTSRC/BKTSRC/IDataManager.cs
@@ -24,6 +24,34 @@ namespace BKTSRC
 		/// <returns></returns>
         public virtual StudyData GetData(string iskillName);
 
+
+        /// <summary>
+		/// Creates ideal observation sequence based on various params
+		/// </summary>
+		/// <param name="modelParam">Params desired</param>
+		/// <param name="starts">starting step</param>
+		/// <param name="observation_sequences">different observation steps</param>
+		/// <param name="resources">resources accessed</param>
+		/// <param name="bigT">no clue tbh</param>
+		/// <returns></returns>
+        protected StudyData CreateIdealModel(
+            ModelParam modelParam,
+            float[] starts,
+            float [] observation_sequences,
+            int[] resources,
+            int bigT)
+		{
+            int[][] all_stateseqs = new int[1, bigT];
+            int[][] all_data = new int[modelParam.num_subparts, bigT];
+            int num_sequences = starts.Length;
+            all_data[0][0] = 0;
+
+            for(int sequence_index = 0; sequence_index < num_sequences; sequence_index++)
+			{
+
+			}
+		}
+
         /// <summary>
 		/// Return a interaction history which should yield specific BKT params
 		/// </summary>
@@ -39,7 +67,12 @@ namespace BKTSRC
 
             int[] resources = Util.initRandom1D(1, num_resources + 1, bigT);
 
-
+            //calculate interaction difference
+            float[] starts = Util.cumSum1D(observation_sequences);
+            for(int i = 0; i< starts.Length; i++)
+			{
+                stars[i] = starts[i] - observation_sequences[i];
+			}
 
 
 		}
